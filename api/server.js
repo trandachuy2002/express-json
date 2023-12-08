c25adc0edc09edf63c33f7b2e94f9c130297b2d3
Trần Đắc Huy <79629158+trandachuy2002@users.noreply.github.com>
2023-12-08 11:47:59 +0700

    updated

--- api/server.js
+++ api/server.js
@@ -13,6 +13,25 @@ server.use(
         "/students/:resource/:id/show": "/:resource/:id",
     })
 );
+server.get((req, res, next) => {
+    if (["POST", "PUT", "PATCH"].includes(req.method)) {
+        if (!validateEmail(req.body.email)) {
+            return res.status(422).send({
+                error: {
+                    email: "Email không đúng định dạng",
+                },
+            });
+        }
+        if (req.body.last_name === "admin") {
+            return res.status(500).send({
+                error: "Server bị lỗi",
+            });
+        }
+    }
+    setTimeout(() => {
+        next();
+    }, DELAY);
+});
 
 server.use(router);
 
