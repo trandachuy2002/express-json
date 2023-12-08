6a0b4d06ab62012dad296f9b7069045f8c5ce366
Trần Đắc Huy <79629158+trandachuy2002@users.noreply.github.com>
2023-12-08 11:39:55 +0700

    updated

--- api/server.js
+++ api/server.js
@@ -6,6 +6,21 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 // Add this before server.use(router)
+
+server.use(
+    jsonServer.rewriter({
+        "/api/*": "/$1",
+        "/students/:resource/:id/show": "/:resource/:id",
+    })
+);
+const validateEmail = (email) => {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+};
+
 server.use((req, res, next) => {
     if (["POST", "PUT", "PATCH"].includes(req.method)) {
         if (!validateEmail(req.body.email)) {
@@ -21,56 +36,23 @@ server.use((req, res, next) => {
             });
         }
     }
-    setTimeout(next, DELAY);
+    setTimeout(() => {
+        next();
+    }, DELAY);
 });
-// const validateEmail = (email) => {
-//     return String(email)
-//         .toLowerCase()
-//         .match(
-//             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-//         );
-// };
-
-// server.use((req, res, next) => {
-//     if (["POST", "PUT", "PATCH"].includes(req.method)) {
-//         if (!validateEmail(req.body.email)) {
-//             return res.status(422).send({
-//                 error: {
-//                     email: "Email không đúng định dạng",
-//                 },
-//             });
-//         }
-//         if (req.body.last_name === "admin") {
-//             return res.status(500).send({
-//                 error: "Server bị lỗi",
-//             });
-//         }
-//     }
-//     setTimeout(() => {
-//         next();
-//     }, DELAY);
-// });
-// router.render = (req, res) => {
-//     let data = res.locals.data;
-//     const { originalUrl } = req;
-//     if (req.method === "GET" && (originalUrl === "/students" || /^\/students\?.*$/.test(originalUrl))) {
-//         data = data.map((student) => ({
-//             id: student.id,
-//             avatar: student.avatar,
-//             last_name: student.last_name,
-//             email: student.email,
-//         }));
-//     }
-//     res.jsonp(data);
-// };
-
-server.use(
-    jsonServer.rewriter({
-        "/api/*": "/$1",
-        "/students/:resource/:id/show": "/:resource/:id",
-    })
-);
-
+router.render = (req, res) => {
+    let data = res.locals.data;
+    const { originalUrl } = req;
+    if (req.method === "GET" && (originalUrl === "/students" || /^\/students\?.*$/.test(originalUrl))) {
+        data = data.map((student) => ({
+            id: student.id,
+            avatar: student.avatar,
+            last_name: student.last_name,
+            email: student.email,
+        }));
+    }
+    res.jsonp(data);
+};
 server.use(router);
 
 server.listen(3000, () => {
