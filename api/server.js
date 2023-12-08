7513f59b78fd74cb94c0112f0432a1270080d51d
Trần Đắc Huy <79629158+trandachuy2002@users.noreply.github.com>
2023-12-08 12:46:47 +0700

    a

--- api/server.js
+++ api/server.js
@@ -3,12 +3,12 @@ const jsonServer = require("json-server");
 const fs = require("fs");
 const os = require("os");
 
-fs.copyFile("db.json", os.tmpdir() + "/db.json", function (err) {
+fs.copyFile("db.json", os.tmpdir() + "db.json", function (err) {
     if (err) console.log(err);
     else console.log("copy file succeed to" + os.tmpdir());
 });
 const server = jsonServer.create();
-const router = jsonServer.router(path.resolve(os.tmpdir() + "/db.json"));
+const router = jsonServer.router(path.resolve(os.tmpdir() + "db.json"));
 // const router = jsonServer.router(path.resolve(os.tmpdir() + "db.json"));
 // const router = jsonServer.router("/tmp/db.json");
 const middlewares = jsonServer.defaults();
