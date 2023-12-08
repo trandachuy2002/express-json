1f82eea44784f6d65fe92cbd7b8ec1893a431b7d
Trần Đắc Huy <79629158+trandachuy2002@users.noreply.github.com>
2023-12-08 12:53:43 +0700

    upda

--- api/server.js
+++ api/server.js
@@ -1,14 +1,10 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require("json-server");
-const fs = require("fs");
-const os = require("os");
 
-fs.copyFile("db.json", os.tmpdir() + "db.json", function (err) {
-    if (err) console.log(err);
-    else console.log("copy file succeed to" + os.tmpdir());
-});
 const server = jsonServer.create();
-const router = jsonServer.router(path.resolve(os.tmpdir() + "db.json"));
+const filePath = path.join("/tmp", "db.json");
+const router = jsonServer.router(filePath);
+
 // const router = jsonServer.router(path.resolve(os.tmpdir() + "db.json"));
 // const router = jsonServer.router("/tmp/db.json");
 const middlewares = jsonServer.defaults();
