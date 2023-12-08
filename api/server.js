44eb7e8442f23adee0369ff56ca1f9400c08fd12
Trần Đắc Huy <79629158+trandachuy2002@users.noreply.github.com>
2023-12-08 11:24:21 +0700

    updated

--- api/server.js
+++ api/server.js
@@ -1,107 +1,109 @@
 // // See https://github.com/typicode/json-server#module
-// const jsonServer = require('json-server')
-// const server = jsonServer.create()
-// const router = jsonServer.router('db.json')
-// const middlewares = jsonServer.defaults()
-
-// server.use(middlewares)
-// // Add this before server.use(router)
-// server.use(jsonServer.rewriter({
-//     '/api/*': '/$1',
-//     '/product/:resource/:id/show': '/:resource/:id'
-// }))
-// server.use(router)
-// server.listen(3000, () => {
-//     console.log('JSON Server is running')
-// })
-
-// // Export the Server API
-// module.exports = server
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
-const PORT = 4000;
-const DELAY = 2000;
-
-const validateEmail = (email) => {
-    return String(email)
-        .toLowerCase()
-        .match(
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        );
-};
-
-// Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
-
-// To handle POST, PUT and PATCH you need to use a body-parser
-// You can use the one used by JSON Server
-server.use(jsonServer.bodyParser);
-server.use((req, res, next) => {
-    if (["POST", "PUT", "PATCH"].includes(req.method)) {
-        if (!validateEmail(req.body.email)) {
-            return res.status(422).send({
-                error: {
-                    email: "Email không đúng định dạng",
-                },
-            });
-        }
-        if (req.body.last_name === "admin") {
-            return res.status(500).send({
-                error: "Server bị lỗi",
-            });
-        }
-    }
-    setTimeout(() => {
-        next();
-    }, DELAY);
-});
+// Add this before server.use(router)
 server.use(
     jsonServer.rewriter({
         "/api/*": "/$1",
-        "/students": "/students/:id",
+        "/product/:resource/:id/show": "/:resource/:id",
     })
 );
-server.delete("/students/:id", (req, res) => {
-    try {
-        const studentId = parseInt(req.params.id, 10);
-
-        // Xóa sinh viên có id cụ thể
-        const removedStudent = router.db.get("students").remove({ id: studentId }).write();
-
-        // Kiểm tra xem có sinh viên nào được xóa không
-        if (removedStudent.length === 0) {
-            return res.status(404).json({ success: false, error: "Không tìm thấy sinh viên" });
-        }
-
-        res.status(200).json({ success: true });
-        console.log("Xóa thành công");
-    } catch (error) {
-        console.error("Lỗi trong quá trình xóa:", error);
-        res.status(500).json({ success: false, error: "Lỗi máy chủ nội bộ" });
-    }
-});
-
-router.render = (req, res) => {
-    let data = res.locals.data;
-    const { originalUrl } = req;
-    if (req.method === "GET" && (originalUrl === "/students" || /^\/students\?.*$/.test(originalUrl))) {
-        data = data.map((student) => ({
-            id: student.id,
-            avatar: student.avatar,
-            last_name: student.last_name,
-            email: student.email,
-        }));
-    }
-    res.jsonp(data);
-};
-server.use({
-    "/*": "/$1",
-});
-// Use default router
 server.use(router);
-server.listen(PORT, () => {
-    console.log(`JSON Server is running at http://localhost:${PORT}`);
+server.listen(3000, () => {
+    console.log("JSON Server is running");
 });
+
+// Export the Server API
+module.exports = server;
+// const jsonServer = require("json-server");
+// const server = jsonServer.create();
+// const router = jsonServer.router("db.json");
+// const middlewares = jsonServer.defaults();
+
+// const PORT = 4000;
+// const DELAY = 2000;
+
+// const validateEmail = (email) => {
+//     return String(email)
+//         .toLowerCase()
+//         .match(
+//             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+//         );
+// };
+
+// // Set default middlewares (logger, static, cors and no-cache)
+// server.use(middlewares);
+
+// // To handle POST, PUT and PATCH you need to use a body-parser
+// // You can use the one used by JSON Server
+// server.use(jsonServer.bodyParser);
+// server.use((req, res, next) => {
+//     if (["POST", "PUT", "PATCH"].includes(req.method)) {
+//         if (!validateEmail(req.body.email)) {
+//             return res.status(422).send({
+//                 error: {
+//                     email: "Email không đúng định dạng",
+//                 },
+//             });
+//         }
+//         if (req.body.last_name === "admin") {
+//             return res.status(500).send({
+//                 error: "Server bị lỗi",
+//             });
+//         }
+//     }
+//     setTimeout(() => {
+//         next();
+//     }, DELAY);
+// });
+// server.use(
+//     jsonServer.rewriter({
+//         "/api/*": "/$1",
+//         "/students": "/students/:id",
+//     })
+// );
+// server.delete("/students/:id", (req, res) => {
+//     try {
+//         const studentId = parseInt(req.params.id, 10);
+
+//         // Xóa sinh viên có id cụ thể
+//         const removedStudent = router.db.get("students").remove({ id: studentId }).write();
+
+//         // Kiểm tra xem có sinh viên nào được xóa không
+//         if (removedStudent.length === 0) {
+//             return res.status(404).json({ success: false, error: "Không tìm thấy sinh viên" });
+//         }
+
+//         res.status(200).json({ success: true });
+//         console.log("Xóa thành công");
+//     } catch (error) {
+//         console.error("Lỗi trong quá trình xóa:", error);
+//         res.status(500).json({ success: false, error: "Lỗi máy chủ nội bộ" });
+//     }
+// });
+
+// router.render = (req, res) => {
+//     let data = res.locals.data;
+//     const { originalUrl } = req;
+//     if (req.method === "GET" && (originalUrl === "/students" || /^\/students\?.*$/.test(originalUrl))) {
+//         data = data.map((student) => ({
+//             id: student.id,
+//             avatar: student.avatar,
+//             last_name: student.last_name,
+//             email: student.email,
+//         }));
+//     }
+//     res.jsonp(data);
+// };
+// server.use({
+//     "/*": "/$1",
+// });
+// // Use default router
+// server.use(router);
+// server.listen(PORT, () => {
+//     console.log(`JSON Server is running at http://localhost:${PORT}`);
+// });
